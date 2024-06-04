1299aa121251bb41d364c9cd5cf0d2ea620ae701
voskreshaumertvbIx <daniilivanchenkocomm@gmail.com>
2024-06-04 12:39:47 +0300

    adding sub-main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './components/header/header';
+import SubMain from './components/sub_main/sub_main';
 
 function App() {
   return (
    <>
    <Header/>
+   <SubMain/>
    </>
   );
 }
---
+++ src/components/sub_main/sub_main.tsx
@@ -0,0 +1,37 @@
+import styles from "./style.module.css";
+
+const clarity = [
+  {
+    name: "Used Cars",
+  },
+  {
+    name: "Any Makes",
+  },
+  {
+    name: "Any Models",
+  },
+  {
+    name: "All Prices",
+  },
+];
+const SubMain = () => {
+  return (
+    <div className={styles.submain_container}>
+      <header className={styles.header_container}>
+        <p className={styles.header_text}>Find cars for sale and for rent near you</p>
+        <h1 className={styles.main_header}>Find Your Dream Car</h1>
+      </header>
+      <div className={styles.search_container}>
+        <ul className={styles.search_list}>
+          {clarity.map(({ name }, index) => {
+            return <li className={styles.search_item} key={index}>{name}</li>;
+          })}
+        </ul>
+        <img className={styles.search_icon}src="./img/Buttonsearch_bar.svg" alt="" />
+      </div>
+      <img className={styles.car_background} src="./img/car_on_slider.svg" alt="car for slider" />
+    </div>
+  );
+};
+
+export default SubMain;
