46f9e22374ceeb0cad24ca8f39937aa50ffeca7f
voskreshaumertvbIx <daniilivanchenkocomm@gmail.com>
2024-06-14 11:56:59 +0300

    add responsive header design

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,57 +1,64 @@
+import React, { useState } from 'react';
 import styles from "./style.module.css";
 
 const links = [
-  {
-    name: "Home",
-  },
-  {
-    name: "Listings",
-  },
-  {
-    name: "Blog",
-  },
-  {
-    name: "Pages",
-  },
-  {
-    name: "About",
-  },
-  {
-    name: "Contact",
-  },
- 
+  { name: "Home" },
+  { name: "Listings" },
+  { name: "Blog" },
+  { name: "Pages" },
+  { name: "About" },
+  { name: "Contact" },
 ];
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className={styles.header_container}>
       <div className={styles.logo_container}>
-        <img
-          className={styles.main_logo}
-          src="./img/logo.svg"
-          alt="site logo"
-        />
-        <img
-          className={styles.phone_logo}
-          src="./img/phone_icon.svg"
-          alt="phone loge"
-        />
+        <img className={styles.main_logo} src="./img/logo.svg" alt="site logo" />
+        <img className={styles.phone_logo} src="./img/phone_icon.svg" alt="phone logo" />
         <p className={styles.phone_number}>+123-78-58-69</p>
       </div>
       <nav className={styles.navigation_container}>
         <ul className={styles.navigation_list}>
           {links.map(({ name }, index) => (
             <li className={styles.nav_text} key={index}>{name}</li> 
-            
           ))}
-       </ul>
-       <div>
-      <img className={styles.signin_icon}src="./img/sign_in.svg" alt="sign_icon" />
-      <a href="" className={styles.nav_text}>Sign in</a>
-      </div>
+          <li>
+            <img className={styles.signin_icon} src="./img/sign_in.svg" alt="sign_icon" />
+            <a href="" className={styles.nav_text}>Sign in</a>
+          </li>
+        </ul>
+        <button className={styles.submit_button}>Submit Listing</button>
+        <div className={styles.burger_menu} onClick={toggleMenu}>
+          <div className={styles.burger_line}></div>
+          <div className={styles.burger_line}></div>
+          <div className={styles.burger_line}></div>
+        </div>
       </nav>
-      
-      <button className={styles.submit_button}>Submit Listing</button>
+      {menuOpen && (
+        <div className={styles.fullscreen_menu}>
+          <div className={styles.close_button} onClick={closeMenu}>&times;</div>
+          <ul className={styles.fullscreen_navigation_list}>
+            {links.map(({ name }, index) => (
+              <li className={styles.fullscreen_nav_text} key={index} onClick={closeMenu}>{name}</li> 
+            ))}
+            <li onClick={closeMenu}>
+              <img className={styles.signin_icon} src="./img/sign_in.svg" alt="sign_icon" />
+              <a href="" className={styles.fullscreen_nav_text}>Sign in</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
